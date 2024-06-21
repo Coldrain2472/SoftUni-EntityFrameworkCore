98e25389f656da8e83ecc38019c8f9436b609cf6
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-21 17:07:30 +0300

    Create StartUp.cs

---
+++ 02. Entity Framework Introduction/02. Database First/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        Console.WriteLine("Connection success!");
+    }
+}
