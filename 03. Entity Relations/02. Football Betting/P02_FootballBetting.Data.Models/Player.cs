d8e394f5d4f9b44e80f6d7e1a1d47cd9d351c1be
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-06-27 10:49:02 +0300

    Update Player.cs

--- 03. Entity Relations/02. Football Betting/P02_FootballBetting.Data.Models/Player.cs
+++ 03. Entity Relations/02. Football Betting/P02_FootballBetting.Data.Models/Player.cs
@@ -35,7 +35,6 @@ public class Player
 
     public ICollection<PlayerStatistic> PlayersStatistics { get; set; }
 
-    // added new Town propperties because of Judge's Unit Tests
     [ForeignKey(nameof(Town))]
     public int TownId { get; set; }
 
