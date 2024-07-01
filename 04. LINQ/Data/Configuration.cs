786bc8bbb85d78a589b2f2c98bb0cc3854ec66e9
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-01 13:47:24 +0300

    Create Configuration.cs

---
+++ 04. LINQ/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=DESKTOP-85U9SF9\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
+    }
+}
