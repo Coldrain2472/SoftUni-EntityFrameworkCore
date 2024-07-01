b89743a232e2637d740730b9c0037dbaf3a1825d
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-01 13:47:41 +0300

    Update Configuration.cs

--- 04. LINQ/Data/Configuration.cs
+++ 04. LINQ/Data/Configuration.cs
@@ -3,6 +3,6 @@
     public static class Configuration
     {
         public static string ConnectionString =
-            @"Server=DESKTOP-85U9SF9\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
+            @"Server=.\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
     }
 }
