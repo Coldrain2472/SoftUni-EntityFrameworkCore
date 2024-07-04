2aaa8b19e8fd4001fc2634e2bbf41ae2a1c144a5
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-04 15:18:55 +0300

    Create Configuration.cs

---
+++ 05. Advanced Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BookShop.Data;
+
+internal class Configuration
+{
+    internal static string ConnectionString
+        => @"Server=DESKTOP-85U9SF9\SQLEXPRESS;Database=BookShop;Integrated Security=True;";
+}
