53f681397aabe6b0b9c1c0329b6eb32ee153d70a
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-09 17:58:49 +0300

    Create ExportCarDistanceDto.cs

---
+++ 07. XML Processing/02. Car Dealer/DTOs/Export/ExportCarDistanceDto.cs
@@ -0,0 +1,21 @@
+﻿namespace CarDealer.DTOs.Export;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+[XmlType("car")]
+public class ExportCarDistanceDto
+{
+    [XmlElement("make")]
+    public string Make { get; set; } = null!;
+
+    [XmlElement("model")]
+    public string Model { get; set; } = null!;
+
+    [XmlElement("traveled-distance")]
+    public long TraveledDistance { get; set; }
+}
