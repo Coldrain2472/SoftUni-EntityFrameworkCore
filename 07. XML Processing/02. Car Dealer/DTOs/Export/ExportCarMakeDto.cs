b3f213bb81df4b616e766c5e460051cf743a8bbe
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-09 17:58:47 +0300

    Create ExportCarMakeDto.cs

---
+++ 07. XML Processing/02. Car Dealer/DTOs/Export/ExportCarMakeDto.cs
@@ -0,0 +1,21 @@
+﻿namespace CarDealer.DTOs.Export;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+[XmlType("car")]
+public class ExportCarMake
+{
+    [XmlAttribute("id")]
+    public int Id { get; set; }
+
+    [XmlAttribute("model")]
+    public string Model { get; set; } = null!;
+
+    [XmlAttribute("traveled-distance")]
+    public long TraveledDistance { get; set; }
+}
