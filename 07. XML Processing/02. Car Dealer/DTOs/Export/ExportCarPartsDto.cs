2bfe0b38ac43c36bf97e358d6489273ab5a378ef
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-09 17:58:45 +0300

    Create ExportCarPartsDto.cs

---
+++ 07. XML Processing/02. Car Dealer/DTOs/Export/ExportCarPartsDto.cs
@@ -0,0 +1,34 @@
+﻿namespace CarDealer.DTOs.Export;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+[XmlType("car")]
+public class ExportCarPartsDto
+{
+    [XmlAttribute("make")]
+    public string Make { get; set; } = null!;
+
+    [XmlAttribute("model")]
+    public string Model { get; set; } = null!;
+
+    [XmlAttribute("traveled-distance")]
+    public long TraveledDistance { get; set; }
+
+    [XmlArray("parts")]
+    public PartsDto[] Parts { get; set; }
+}
+
+[XmlType("part")]
+public class PartsDto
+{
+    [XmlAttribute("name")]
+    public string Name { get; set; } = null!;
+
+    [XmlAttribute("price")]
+    public decimal Price { get; set; }
+}
