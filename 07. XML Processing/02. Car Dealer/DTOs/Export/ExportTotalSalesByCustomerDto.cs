769b423a1f3f24783733f459440796e3f8a7f788
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-09 17:58:39 +0300

    Create ExportTotalSalesByCustomerDto.cs

---
+++ 07. XML Processing/02. Car Dealer/DTOs/Export/ExportTotalSalesByCustomerDto.cs
@@ -0,0 +1,21 @@
+﻿namespace CarDealer.DTOs.Export;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+[XmlType("customer")]
+public class ExportTotalSalesByCustomerDto
+{
+    [XmlAttribute("full-name")]
+    public string FullName { get; set; }
+
+    [XmlAttribute("bought-cars")]
+    public int BoughtCars { get; set; }
+
+    [XmlAttribute("spent-money")]
+    public string SpentMoney { get; set; }
+}
