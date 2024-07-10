fd0fa8a157817b3b01b713b250072132259038b8
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-10 12:15:24 +0300

    Create ImportProductDto.cs

---
+++ 07. XML Processing/01. Product Shop/DTOs/Import/ImportProductDto.cs
@@ -0,0 +1,25 @@
+﻿namespace ProductShop.DTOs.Import;
+
+using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+[XmlType("Product")]
+public class ImportProductDto
+{
+    [XmlElement("name")]
+    public string Name { get; set; } = null!;
+
+    [XmlElement("price")]
+    public decimal Price { get; set; }
+
+    [XmlElement("sellerId")]
+    public int SellerId { get; set; }
+
+    [XmlElement("buyerId")]
+    public int? BuyerId { get; set; }
+}
