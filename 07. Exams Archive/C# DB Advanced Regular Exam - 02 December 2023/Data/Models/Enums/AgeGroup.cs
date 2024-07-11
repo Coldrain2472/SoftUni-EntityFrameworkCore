611f5141cc9678f91630dee88b9560d1206013cd
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-11 14:56:12 +0300

    Create AgeGroup.cs

---
+++ 07. Exams Archive/C# DB Advanced Regular Exam - 02 December 2023/Data/Models/Enums/AgeGroup.cs
@@ -0,0 +1,14 @@
+﻿namespace Medicines.Data.Models.Enums;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum AgeGroup
+{
+    Child = 0, 
+    Adult = 1, 
+    Senior = 2
+}
