87c8f8650a81ce092fd8d4dfcfe2afcbe75578bf
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-11 14:55:57 +0300

    Create ExportPatientDto.cs

---
+++ 07. Exams Archive/C# DB Advanced Regular Exam - 02 December 2023/DataProcessor/ExportDtos/ExportPatientDto.cs
@@ -0,0 +1,43 @@
+﻿namespace Medicines.DataProcessor.ExportDtos;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+[XmlType("Patient")]
+public class ExportPatientDto
+{
+    [XmlAttribute("Gender")]
+    public string Gender { get; set; }
+
+    [XmlElement("Name")]
+    public string FullName { get; set; }
+
+    [XmlElement("AgeGroup")]
+    public string AgeGroup { get; set; }
+
+    [XmlArray("Medicines")]
+    public ExportMedicineDto[] Medicines { get; set; }
+}
+
+[XmlType("Medicine")]
+public class ExportMedicineDto
+{
+    [XmlAttribute("Category")]
+    public string Category { get; set; }
+
+    [XmlElement("Name")]
+    public string Name { get; set; }
+
+    [XmlElement("Price")]
+    public string Price { get; set; }
+
+    [XmlElement("Producer")]
+    public string Producer { get; set; }
+
+    [XmlElement("BestBefore")]
+    public string BestBefore { get; set; }
+}
