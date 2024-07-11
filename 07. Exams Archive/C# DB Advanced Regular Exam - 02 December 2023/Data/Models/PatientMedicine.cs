ff50f403770e611d180e589a3928719dba17403f
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-11 14:56:03 +0300

    Create PatientMedicine.cs

---
+++ 07. Exams Archive/C# DB Advanced Regular Exam - 02 December 2023/Data/Models/PatientMedicine.cs
@@ -0,0 +1,21 @@
+﻿namespace Medicines.Data.Models;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PatientMedicine
+{
+    [Required]
+    public int PatientId { get; set; }
+
+    public virtual Patient? Patient { get; set; }
+
+    [Required]
+    public int MedicineId {  get; set; }
+
+    public virtual Medicine? Medicine { get; set; }
+}
