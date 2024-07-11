af470ea4aec249694533411ac1c2800cb88f9bdf
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-11 14:56:10 +0300

    Create Category.cs

---
+++ 07. Exams Archive/C# DB Advanced Regular Exam - 02 December 2023/Data/Models/Enums/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace Medicines.Data.Models.Enums;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Category
+{
+    Analgesic = 0, 
+    Antibiotic = 1, 
+    Antiseptic = 2, 
+    Sedative = 3, 
+    Vaccine = 4
+}
