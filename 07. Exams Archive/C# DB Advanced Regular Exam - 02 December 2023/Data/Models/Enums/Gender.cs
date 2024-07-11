467953b6c071d38d8f23f329750ce988022d6b02
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-11 14:56:08 +0300

    Create Gender.cs

---
+++ 07. Exams Archive/C# DB Advanced Regular Exam - 02 December 2023/Data/Models/Enums/Gender.cs
@@ -0,0 +1,13 @@
+﻿namespace Medicines.Data.Models.Enums;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Gender
+{
+    Male = 0, 
+    Female = 1
+}
