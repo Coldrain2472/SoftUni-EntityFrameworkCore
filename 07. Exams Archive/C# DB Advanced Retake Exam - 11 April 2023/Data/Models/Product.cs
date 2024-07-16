d6b442a7f2447d808342abec35b8ca9f775ce65e
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-16 13:38:36 +0300

    Create Product.cs

---
+++ 07. Exams Archive/C# DB Advanced Retake Exam - 11 April 2023/Data/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using Invoices.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoices.Data.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            this.ProductsClients = new HashSet<ProductClient>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Range(5.00, 1000.00)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+
+        public virtual ICollection<ProductClient> ProductsClients { get; set; }
+    }
+}
