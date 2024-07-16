a5239e91d622fa910f27b614689081abab9879f9
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-16 13:38:27 +0300

    Create ImportInvoiceDto.cs

---
+++ 07. Exams Archive/C# DB Advanced Retake Exam - 11 April 2023/DataProcessor/ImportDto/ImportInvoiceDto.cs
@@ -0,0 +1,40 @@
+﻿using Invoices.Data.Models.Enums;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoices.DataProcessor.ImportDto
+{
+    public class ImportInvoiceDto
+    {
+        [JsonProperty("Number")]
+        [Required]
+        [Range(1000000000, 1500000000)]
+        public int Number {  get; set; }
+
+        [JsonProperty("IssueDate")]
+        [Required]
+        public string IssueDate {  get; set; } = null!;
+
+        [JsonProperty("DueDate")]
+        [Required]
+        public string DueDate { get; set; } = null!;
+
+        [JsonProperty("Amount")]
+        [Required]
+        public decimal Amount { get; set; }
+
+        [JsonProperty("CurrencyType")]
+        [Required]
+        [Range(0, 2)]
+        public int CurrencyType { get; set; }
+
+        [JsonProperty("ClientId")]
+        [Required]
+        public int ClientId { get; set; }
+    }
+}
