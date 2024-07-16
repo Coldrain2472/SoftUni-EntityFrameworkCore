8d876cfadab8ff1a0b4f72f191871aff042ff3cc
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-16 13:38:29 +0300

    Create ImportClientAddressDto.cs

---
+++ 07. Exams Archive/C# DB Advanced Retake Exam - 11 April 2023/DataProcessor/ImportDto/ImportClientAddressDto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Invoices.DataProcessor.ImportDto
+{
+    [XmlType("Client")]
+    public class ImportClientDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(10)]
+        [MaxLength(25)]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("NumberVat")]
+        [Required]
+        [MinLength(10)]
+        [MaxLength(15)]
+        public string NumberVat { get; set; } = null!;
+
+        [XmlArray("Addresses")]
+        public ImportClientAddressDto[] Addresses {  get; set; }
+    }
+
+
+    [XmlType("Address")]
+    public class ImportClientAddressDto
+    {
+        [XmlElement("StreetName")]
+        [Required]
+        [MinLength(10)]
+        [MaxLength(20)]
+        public string StreetName { get; set; } = null!;
+
+        [XmlElement("StreetNumber")]
+        [Required]
+        public int StreetNumber {  get; set; }
+
+        [XmlElement("PostCode")]
+        [Required]
+        public string PostCode { get; set; } = null!;
+
+        [XmlElement("City")]
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
+        public string City { get; set; } = null!;
+
+        [XmlElement("Country")]
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
+        public string Country { get; set; } = null!;
+    }
+}
