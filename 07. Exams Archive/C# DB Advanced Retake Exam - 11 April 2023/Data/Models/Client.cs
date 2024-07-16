2342aa7d5d3dd12029ce78dc74e45d3bed66afd3
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-16 13:38:42 +0300

    Create Client.cs

---
+++ 07. Exams Archive/C# DB Advanced Retake Exam - 11 April 2023/Data/Models/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoices.Data.Models
+{
+    public class Client
+    {
+        public Client()
+        {
+            this.Invoices = new HashSet<Invoice>();
+            this.Addresses = new HashSet<Address>();
+            this.ProductsClients = new HashSet<ProductClient>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(15)]
+        public string NumberVat { get; set; } = null!;
+
+        public virtual ICollection<Invoice> Invoices { get; set; }
+
+        public virtual ICollection<Address> Addresses { get; set; }
+
+        public virtual ICollection<ProductClient> ProductsClients { get; set; }
+    }
+}
