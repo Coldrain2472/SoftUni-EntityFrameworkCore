70c3b19520c3082299480abbdd167ae82dfe913b
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-16 11:57:01 +0300

    Create Serializer.cs

---
+++ 07. Exams Archive/C# DB Advanced Retake Exam - 15 August 2022/DataProcessor/Serializer.cs
@@ -0,0 +1,67 @@
+﻿namespace Trucks.DataProcessor
+{
+    using Data;
+    using Newtonsoft.Json;
+    using Trucks.DataProcessor.ExportDto;
+    using Trucks.Utilities;
+
+    public class Serializer
+    {
+        public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
+        {
+            var despatchers = context.Despatchers
+                .Where(d => d.Trucks.Any())
+                .Select(d => new ExportDespatcherDto
+                {
+                    TrucksCount = d.Trucks.Count,
+                    Name = d.Name,
+                    Trucks = d.Trucks
+                    .Select(t => new ExportTruckDto
+                    {
+                        RegistrationNumber = t.RegistrationNumber,
+                        MakeType = t.MakeType.ToString()
+                    })
+                    .OrderBy(t => t.RegistrationNumber)
+                    .ToArray()
+                })
+                .OrderByDescending(t => t.TrucksCount)
+                .ThenBy(t => t.Name)
+                .ToArray();
+
+            XmlHelper xmlHelper = new XmlHelper();
+
+            return xmlHelper.Serialize<ExportDespatcherDto[]>(despatchers, "Despatchers");
+        }
+
+        public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
+        {
+            var clients = context.Clients
+                .Where(c => c.ClientsTrucks.Any(ct => ct.Truck.TankCapacity >= capacity))
+                .AsEnumerable()
+                .Select(c => new
+                {
+                    Name = c.Name,
+                    Trucks = c.ClientsTrucks
+                    .Where(ct => ct.Truck.TankCapacity >= capacity)
+                    .Select(ct => new
+                    {
+                        TruckRegistrationNumber = ct.Truck.RegistrationNumber,
+                        VinNumber = ct.Truck.VinNumber,
+                        TankCapacity = ct.Truck.TankCapacity,
+                        CargoCapacity = ct.Truck.CargoCapacity,
+                        CategoryType = ct.Truck.CategoryType.ToString(),
+                        MakeType = ct.Truck.MakeType.ToString()
+                    })
+                    .OrderBy(t => t.MakeType)
+                    .ThenByDescending(t => t.CargoCapacity)
+                    .ToArray()
+                })
+                .OrderByDescending(c => c.Trucks.Length)
+                .ThenBy(c => c.Name)
+                .Take(10)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(clients, Formatting.Indented);
+        }
+    }
+}
