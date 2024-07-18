2f5c1ff943d8e0cf4116ec4f727c5a0d62cd791f
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-18 12:37:56 +0300

    Create ImportCreatorDto.cs

---
+++ 07. Exams Archive/C# DB Advanced Exam - 01 April 2023/DataProcessor/ImportDto/ImportCreatorDto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    [XmlType("Creator")]
+    public class ImportCreatorDto
+    {
+        [XmlElement("FirstName")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(7)]
+        public string FirstName { get; set; } = null!;
+
+        [XmlElement("LastName")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(7)]
+        public string LastName { get; set; } = null!;
+
+        [XmlArray("Boardgames")]
+        public ImportBoardgameDto[] Boardgames { get; set; } = null!;
+    }
+
+    [XmlType("Boardgame")]
+    public class ImportBoardgameDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(10)]
+        [MaxLength(20)]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("Rating")]
+        [Required]
+        [Range(1, 10.00)]
+        public double Rating { get; set; }
+
+        [XmlElement("YearPublished")]
+        [Required]
+        [Range(2018, 2023)]
+        public int YearPublished { get; set; }
+
+        [XmlElement("CategoryType")]
+        [Required]
+        [Range(0, 4)]
+        public int CategoryType { get; set; }
+
+        [XmlElement("Mechanics")]
+        [Required]
+        public string Mechanics {  get; set; } = null!;
+    }
+}
