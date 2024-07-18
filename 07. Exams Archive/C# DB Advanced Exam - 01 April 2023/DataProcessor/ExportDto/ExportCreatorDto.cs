e731cef9c0774dcb231a0b2f2b86de146e468a63
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-18 12:37:57 +0300

    Create ExportCreatorDto.cs

---
+++ 07. Exams Archive/C# DB Advanced Exam - 01 April 2023/DataProcessor/ExportDto/ExportCreatorDto.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ExportDto
+{
+    [XmlType("Creator")]
+    public class ExportCreatorDto
+    {
+        [XmlAttribute("BoardgamesCount")]
+        public int BoardgamesCount { get; set; }
+
+        [XmlElement("CreatorName")]
+        public string CreatorName { get; set; }
+
+        [XmlArray("Boardgames")]
+        public ExportBoardgameDto[] Boardgames { get; set; } = null!;
+    }
+
+    [XmlType("Boardgame")]
+    public class ExportBoardgameDto
+    {
+        [XmlElement("BoardgameName")]
+        public string BoardgameName { get; set; }
+
+        [XmlElement("BoardgameYearPublished")]
+        public int BoardgameYearPublished { get; set; }
+    }
+}
