5f0a82eb73dad44e2d3c6d439455ef5854c0e9ce
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-18 10:59:36 +0300

    Create ImportDistrictDto.cs

---
+++ 07. Exams Archive/C# DB Advanced Retake Exam - 11 December 2023/DataProcessor/ImportDtos/ImportDistrictDto.cs
@@ -0,0 +1,65 @@
+﻿using Cadastre.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Cadastre.DataProcessor.ImportDtos
+{
+    [XmlType("District")]
+    public class ImportDistrictDto
+    {
+        [XmlAttribute("Region")]
+        [Required]
+        public string Region { get; set; } = null!;
+
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(80)]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("PostalCode")]
+        [Required]
+        [MinLength(8)]
+        [MaxLength(8)]
+        [RegularExpression(@"^[A-Z]{2}-\d{5}$")]
+        public string PostalCode { get; set; } = null!;
+
+        [XmlArray("Properties")]
+        public ImportPropertiesDto[] Properties { get; set; } = null!;
+    }
+
+    [XmlType("Property")]
+    public class ImportPropertiesDto
+    {
+        [XmlElement("PropertyIdentifier")]
+        [Required]
+        [MinLength(16)]
+        [MaxLength(20)]
+        public string PropertyIdentifier { get; set; } = null!;
+
+        [XmlElement("Area")]
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Area { get; set; }
+
+        [XmlElement("Details")]
+        [MinLength(5)]
+        [MaxLength(500)]
+        public string? Details { get; set; }
+
+        [XmlElement("Address")]
+        [Required]
+        [MinLength(5)]
+        [MaxLength(200)]
+        public string Address { get; set; } = null!;
+
+        [XmlElement("DateOfAcquisition")]
+        [Required]
+        public string DateOfAcquisition { get; set; } = null!;
+    }
+}
