c24dc910946d3d69cd33c2eaf115ce5f5521703d
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-19 11:31:51 +0300

    Create ImportTeamDto.cs

---
+++ 07. Exams Archive/C# DB Advanced Exam - 06 August 2022/DataProcessor/ImportDto/ImportTeamDto.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    public class ImportTeamDto
+    {
+        [JsonProperty("Name")]
+        [Required]
+        [MinLength(3)]
+        [MaxLength(40)]
+        [RegularExpression(@"^[a-zA-Z0-9\s.\-]{3,40}$")]
+        public string Name { get; set; } = null!;
+
+        [JsonProperty("Nationality")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Nationality { get; set; } = null!;
+
+        [JsonProperty("Trophies")]
+        [Required]
+        public int Trophies { get; set; }
+
+        [JsonProperty("Footballers")]
+        public int[] FootballersIds {  get; set; } = null!;
+    }
+}
