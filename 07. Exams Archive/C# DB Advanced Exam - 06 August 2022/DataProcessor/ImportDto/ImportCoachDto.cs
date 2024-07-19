720b14ca4496921c17f5586983326f4b124bc788
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-19 11:31:53 +0300

    Create ImportCoachDto.cs

---
+++ 07. Exams Archive/C# DB Advanced Exam - 06 August 2022/DataProcessor/ImportDto/ImportCoachDto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlType("Coach")]
+    public class ImportCoachDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("Nationality")]
+        [Required]
+        public string Nationality { get; set; } = null!;
+
+        [XmlArray("Footballers")]
+        public ImportFootballerDto[] Footballers { get; set; } = null!;
+    }
+
+    [XmlType("Footballer")]
+    public class ImportFootballerDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("ContractStartDate")]
+        [Required]
+        public string ContractStartDate { get; set; } = null!;
+
+        [XmlElement("ContractEndDate")]
+        [Required]
+        public string ContractEndDate { get; set; } = null!;
+
+        [XmlElement("BestSkillType")]
+        [Required]
+        [Range(0, 4)]
+        public int BestSkillType { get; set; }
+
+        [XmlElement("PositionType")]
+        [Required]
+        [Range(0, 3)]
+        public int PositionType { get; set; }
+    }
+}
