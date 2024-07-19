d45e891077286e374242373cf4c893e23ea80a7c
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-07-19 11:31:56 +0300

    Create Deserializer.cs

---
+++ 07. Exams Archive/C# DB Advanced Exam - 06 August 2022/DataProcessor/Deserializer.cs
@@ -0,0 +1,164 @@
+﻿namespace Footballers.DataProcessor
+{
+    using Footballers.Data;
+    using Footballers.Data.Models;
+    using Footballers.Data.Models.Enums;
+    using Footballers.DataProcessor.ImportDto;
+    using Footballers.Utilities;
+    using Newtonsoft.Json;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.Text;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedCoach
+            = "Successfully imported coach - {0} with {1} footballers.";
+
+        private const string SuccessfullyImportedTeam
+            = "Successfully imported team - {0} with {1} footballers.";
+
+        public static string ImportCoaches(FootballersContext context, string xmlString)
+        {
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportCoachDto[] coachDtos = xmlHelper.Deserialize<ImportCoachDto[]>(xmlString, "Coaches");
+
+            HashSet<Coach> validCoaches = new HashSet<Coach>();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var coachDto in coachDtos)
+            {
+                if (!IsValid(coachDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                if (String.IsNullOrEmpty(coachDto.Nationality))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                
+                Coach coach = new Coach()
+                {
+                    Name = coachDto.Name,
+                    Nationality = coachDto.Nationality
+                };
+
+                foreach (var footballerDto in coachDto.Footballers)
+                {
+                    DateTime contractStartDate;
+                    DateTime contractEndDate;
+
+                    if (!IsValid(footballerDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+                    if (!DateTime.TryParseExact(footballerDto.ContractStartDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out contractStartDate)
+                        || !DateTime.TryParseExact(footballerDto.ContractEndDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out contractEndDate))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    if (contractStartDate > contractEndDate)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Footballer footballer = new Footballer()
+                    {
+                        Name = footballerDto.Name,
+                        ContractStartDate = contractStartDate,
+                        ContractEndDate = contractEndDate,
+                        BestSkillType = (BestSkillType)footballerDto.BestSkillType,
+                        PositionType = (PositionType)footballerDto.PositionType
+                    };
+                    
+                    coach.Footballers.Add(footballer);
+                }
+               
+                validCoaches.Add(coach);
+                sb.AppendLine(String.Format(SuccessfullyImportedCoach, coach.Name, coach.Footballers.Count));
+            }
+            
+            context.Coaches.AddRange(validCoaches);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportTeams(FootballersContext context, string jsonString)
+        {
+            ImportTeamDto[] teamDtos = JsonConvert.DeserializeObject<ImportTeamDto[]>(jsonString);
+
+            HashSet<Team> validTeams = new HashSet<Team>();
+
+            StringBuilder sb = new StringBuilder();
+
+            int[] footballersIds = context.Footballers
+                .Select(f => f.Id)
+                .ToArray();
+
+            foreach (var teamDto in teamDtos)
+            {
+                if (!IsValid(teamDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                if (teamDto.Trophies == 0 || String.IsNullOrEmpty(teamDto.Nationality))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                
+                Team team = new Team()
+                {
+                    Name = teamDto.Name,
+                    Nationality = teamDto.Nationality,
+                    Trophies = teamDto.Trophies
+                };
+
+                foreach (var footballerId in teamDto.FootballersIds.Distinct())
+                {
+                    if (!footballersIds.Contains(footballerId))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+                    
+                    team.TeamsFootballers.Add(new TeamFootballer()
+                    {
+                        TeamId = team.Id,
+                        FootballerId = footballerId
+                    });
+                }
+                
+                validTeams.Add(team);
+                sb.AppendLine(String.Format(SuccessfullyImportedTeam, team.Name, team.TeamsFootballers.Count));
+            }
+            
+            context.Teams.AddRange(validTeams);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
