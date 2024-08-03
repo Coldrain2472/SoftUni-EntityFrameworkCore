ca66c1d2a47fcfac45a529a59ea2357a16c761f6
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-08-03 14:36:37 +0300

    Create ExportGuideDto.cs

---
+++ Regular Exam/DataProcessor/ExportDtos/ExportGuideDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TravelAgency.DataProcessor.ExportDtos
+{
+    [XmlType("Guide")]
+    public class ExportGuideDto
+    {
+        public string FullName { get; set; }
+
+        public List<ExportTourPackageDto> TourPackages {  get; set; }
+    }
+
+    [XmlType("TourPackage")]
+    public class ExportTourPackageDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
