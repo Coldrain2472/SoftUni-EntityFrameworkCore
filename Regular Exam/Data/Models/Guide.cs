25115630aabd0bfa1d29a12ce0200534c7f83d8c
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-08-03 14:37:06 +0300

    Create Guide.cs

---
+++ Regular Exam/Data/Models/Guide.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Data.Models.Enums;
+
+namespace TravelAgency.Data.Models
+{
+    public class Guide
+    {
+        public Guide()
+        {
+            this.TourPackagesGuides = new HashSet<TourPackageGuide>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        public Language Language { get; set; }
+
+        public virtual ICollection<TourPackageGuide> TourPackagesGuides {  get; set; }
+    }
+}
