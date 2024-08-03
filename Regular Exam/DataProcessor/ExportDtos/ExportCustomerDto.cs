fb4ab47ff851a0d83471ce79b708a5c9557b103d
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-08-03 14:36:39 +0300

    Create ExportCustomerDto.cs

---
+++ Regular Exam/DataProcessor/ExportDtos/ExportCustomerDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.DataProcessor.ExportDtos
+{
+    public class ExportCustomerDto
+    {
+        public string FullName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public List<ExportBookingDto> Bookings {  get; set; }
+    }
+
+    public class ExportBookingDto
+    {
+        public string TourPackageName { get; set; }
+
+        public string Date {  get; set; }
+    }
+}
