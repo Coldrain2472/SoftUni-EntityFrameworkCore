900f245ea2dbe6cab5dd37f9f5e63cc25ffe8aa2
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-08-03 14:37:09 +0300

    Create Customer.cs

---
+++ Regular Exam/Data/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Data.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            this.Bookings = new HashSet<Booking>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string FullName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MaxLength(13)]
+        public string PhoneNumber { get; set; } = null!;
+
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+}
